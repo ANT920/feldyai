0876bc47cc93cfeb98e2b93ee38856dee8f92dd1
ANT920 <antonmarnopolskij@gmail.com>
2025-02-02 00:59:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
@@ -11,6 +11,12 @@ trainer.train("chatterbot.corpus.russian")
 @app.route('/')
 def home():
     return "Привет! Я FeldyAI 2.0. Как я могу помочь вам сегодня?"
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    user_input = request.form['text']
+    bot_response = bot.get_response(user_input)
+    return str(bot_response)
 
 if __name__ == "__main__":
     app.run()
